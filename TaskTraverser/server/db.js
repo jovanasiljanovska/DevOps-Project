079385eb13bf00089aa8de7ca03fa3f491e6d916
jovanasiljanovska <jovana.siljanovska@students.finki.ukim.mk>
2026-02-07 17:35:10 +0100

    update db.js to work with backend-config

--- TaskTraverser/server/db.js
+++ TaskTraverser/server/db.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST || "db",
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || "todotraverse"
+  user: process.env.POSTGRES_USER,
+  password: String(process.env.POSTGRES_PASSWORD),
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.POSTGRES_DB
 });
 
 module.exports = pool;
+
